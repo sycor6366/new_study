c411f6df14845f1dfcadeac3cf1cd8015e2b1be2
shiyuan.liu <451470004@qq.com>
2018-05-27 16:45:28 +0800

    test

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choicd(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+class PhoneBook(models.Model):
+    tel = models.IntegerField(default='')
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "name:{},tel:{}".format(self.name,self.tel)

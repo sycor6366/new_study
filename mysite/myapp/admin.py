4ef2835d070aefcabb5be8ca111360c88b87b91d
shiyuan.liu <451470004@qq.com>
2018-05-27 17:25:19 +0800

    test

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question,Choicd,PhoneBook
+
+admin.site.register(Question)
+admin.site.register(Choicd)
+admin.site.register(PhoneBook)
+

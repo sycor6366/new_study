2ddc88e576f57030420cbb43262bea618cef05c9
shiyuan.liu <451470004@qq.com>
2018-06-10 13:48:11 +0800

    test

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('',views.index, name = 'index')]
+urlpatterns = [
+    path('',views.index, name = 'index'),
+    # ex:/myapp/1/
+    path('<int:question_id>',views.detail,name = 'detail'),
+    # ex: /myapp/5/results/
+    path('<int:question_id>/results',views.results,name = 'results'),
+    # ex: /myapp/5/vote/
+    path('<int:question_id>/vote',views.vote,name='vote')
+]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import  Question
 
 
 def index(request) :
-    return HttpResponse("hello world you are at my app index,123123....")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request,question_id):
+    return HttpResponse('you are looking at question {}'.format(question_id))
+
+def results(request,question_id):
+    request = 'you are looking at the results of question %s'
+    return HttpResponse(request % question_id)
+
+def vote(request , question_id):
+    return  HttpResponse('you are voting on question {}'.format(question_id))

a293418fb53ead1f357454ea1a3ce1611ae71adb
weixin_40546338 <451470004@qq.com>
2018-08-30 19:01:36 +0800

    add autotest

--- locallibray/locallibray/settings.py
+++ locallibray/locallibray/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE':'djiango.db.backends.mysql',
+        'ENGINE':'django.db.backends.mysql',
         'NAME':'locallibrary_lsy',
-        'USER':'ROOT',
+        'USER':'root',
         'PASSWORD':'123456',
         'HOST':'127.0.0.1',
         'PORT':'3306'
